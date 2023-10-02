108c64cba28c8937e58a86e8efbc4e21516d31dc
Mason Kuenneke <makuenneke@gmail.com>
2023-10-02 14:57:31 -0500

    Fixed some things

--- mason-kuenneke/src/app/heroes/heroes.component.ts
+++ mason-kuenneke/src/app/heroes/heroes.component.ts
@@ -9,18 +9,21 @@ import {HeroService} from "../hero.service";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   heroes: Hero[] = [];
 
-selectedHero?: Hero;
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
-constructor(private heroService: HeroService) {
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
-}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  constructor(private heroService: HeroService) {
+
+  }
 }
